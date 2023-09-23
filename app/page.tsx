e3cadc2ed25ac2ef9e191e67990d05f5835de605
joseph-banaag <josephrbanaag51@gmail.com>
2023-09-23 22:24:40 +0800

    added testing of useContext

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,10 @@ export default function Home() {
   return (
     <main className="w-full flex-col flex justify-center items-center min-h-screen gap-11">
       <Provider store={store}>
+        <h1 className="text-3xl text-white">Below is for the Redux state management</h1>
         <Counter />
         <h1 className="text-3xl text-white">Below is for the React hook useContext</h1>
-        <p className="text-white">Here is the value of the data state: {data}</p>
+        <p className="text-white flex justify-center items-center">Here is the value of the data state from the receiver component: <span className="border w-[10em] h-auto px-5 py-5 flex justify-center items-center ms-2">{data}</span></p>
         <DataStateValue />
       </Provider>
     </main>
